92e78123fcfc8aef97c3daf2fb66deb8c3b68842
Sergo <kisluy19@ukr.net>
2020-02-10 09:45:39 +0200

    Добавьте файлы проекта.

---
+++ LNAU24/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LNAU24.controls;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LNAU24
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+        
+    }
+}
